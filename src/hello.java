2055a93554cd53274f82c9c73202635e525e0b97
Forkeep <13126593533@163.com>
2020-04-15 18:49:31 +0800

    == 和 .equals()

--- src/hello.java
+++ src/hello.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 
 public class hello {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("input name :");
-        String name = scanner.nextLine();
-        System.out.print("input age :");
-        int age = scanner.nextInt();
-        System.out.printf("Hi, %s, you are %d\n", name, age); // 格式化输出
+        String s1 = "hello";
+        String s2 = "HELLO".toLowerCase();
+        System.out.println(s1);
+        System.out.println(s2);
+        // .equals()判断引用类型变量内容是否相等
+        // == 判断值类型变量内偶然那个是否相等  或者  引用类型的引用地址是否相等
+        if (s1.equals(s2)) {
+            System.out.println("s1 equals s2");
+        } else {
+            System.out.println("s1 not equals s2");
+        }
 
     }
 

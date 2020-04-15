ba64a3f00369d9891fbe2c45648d76f143a5a6c3
Forkeep <13126593533@163.com>
2020-04-15 19:03:09 +0800

    命令行参数输入 和 获取

--- src/hello.java
+++ src/hello.java
@@ -3,13 +3,9 @@ import java.util.Scanner;
 
 public class hello {
     public static void main(String[] args) {
-        int[] ns = { 28, 12, 89, 73, 65, 18, 96, 50, 8, 36 };
-        var bs = ns;
-        Arrays.sort(ns);
-        System.out.println(Arrays.toString(ns));
-        System.out.println(Arrays.toString(bs));
-
-
+        for (String arg : args) {
+            System.out.println(arg);
+        }
     }
 
 }

8f5d4ed33694d1667cc14f3e6c5b78d341229748
Forkeep <13126593533@163.com>
2020-04-15 18:51:23 +0800

    避免null.equals()使用短路符号&&

--- src/hello.java
+++ src/hello.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class hello {
     public static void main(String[] args) {
-        String s1 = "hello";
+        String s1 = null;
         String s2 = "HELLO".toLowerCase();
         System.out.println(s1);
         System.out.println(s2);
         // .equals()判断引用类型变量内容是否相等
         // == 判断值类型变量内偶然那个是否相等  或者  引用类型的引用地址是否相等
-        if (s1.equals(s2)) {
+        if (s1 != null && s1.equals(s2)) {
             System.out.println("s1 equals s2");
         } else {
             System.out.println("s1 not equals s2");
